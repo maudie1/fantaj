2433f882613ad0eccdbd4b2fa0a27d8095c9ee02
maudie1 <4rgn2004@gmail.com>
2026-05-30 12:15:42 +0900

    로그인 색인

---
+++ js/supabaseClient.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://zzrmkxilrxnmvgqkvman.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_TA1sNZ1dpx64MZfMFHESvw_5B4EMrTg";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
